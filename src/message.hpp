9c3c23fab46c56e46c13b94129e4a72fffad5bbc
rendayigit <renda.yigit@gmail.com>
2024-11-21 16:38:38 +0300

    added command word 2 info

--- src/message.hpp
+++ src/message.hpp
@@ -10,9 +10,9 @@
 class Message {
 public:
   explicit Message(U16BIT rt, U16BIT sa, U16BIT rtRx, U16BIT saRx, U16BIT wc, char bus, const char *type,
-                   std::string time, const U16BIT *data, bool noRes)
+                   std::string time, const U16BIT *data, bool cmdWord2, bool noRes)
       : m_rt(rt), m_sa(sa), m_rtRx(rtRx), m_saRx(saRx), m_wc(wc), m_bus(bus), m_type(type), m_time(std::move(time)),
-        m_noRes(noRes) {
+        m_cmdWord2(cmdWord2), m_noRes(noRes) {
     for (int i = 0; i < RT_SA_MAX_COUNT; i++) {
       std::ostringstream stream;
       stream << std::setfill('0') << std::setw(4) << std::hex << std::uppercase
@@ -30,6 +30,7 @@ public:
   std::string getType() const { return m_type; }
   std::string getTime() const { return m_time; }
   std::vector<std::string> getData() const { return m_data; }
+  bool isCmdWord2() const { return m_cmdWord2; }
   bool isResponded() const { return not m_noRes; }
 
 private:
@@ -42,6 +43,7 @@ private:
   std::string m_type;
   std::string m_time;
   std::vector<std::string> m_data;
+  bool m_cmdWord2;
   bool m_noRes;
 };
 
