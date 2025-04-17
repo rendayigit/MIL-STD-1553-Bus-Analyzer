cd14c40a09b97d4a8b4b008a212798e73900e04f
rendayigit <renda.yigit@gmail.com>
2025-04-17 16:54:08 +0300

    implementing rt logic

--- src/rt/main.cpp
+++ src/rt/main.cpp
@@ -1,10 +1,19 @@
 #include "rt.hpp"
 
-int main(int  /*argc*/, char**  /*argv*/) {
-    RT::getInstance().start(0);
+#include <iostream>
 
-    RT::getInstance().setRt(1, 1, 5, ACE_BCCTRL_CHL_A, { "AAAA", "BBBB", "CCCC", "1234", "FFFF" });
-    RT::getInstance().activateRt(1, 1);
-    
-    return 0;
+int main(int /*argc*/, char ** /*argv*/) {
+  RT::getInstance().start(0);
+
+  // RT::getInstance().setRt(1, 1, 5, ACE_BCCTRL_CHL_A, { "AAAA", "BBBB", "CCCC", "1234", "FFFF" });
+  RT::getInstance().activateRt(1, 1);
+
+  // Wait for user input to stop
+  std::cout << "\nPress enter to stop\n\n";
+  char input = 0;
+  while (input != '\n') {
+    std::cin.get(input);
+  }
+
+  return 0;
 }
